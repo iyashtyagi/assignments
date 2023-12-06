51285002895d2e4e251f91a0c5cf5301b34fd2ce
Yash Tyagi <contactyashtyagi@gmail.com>
2023-12-06 09:38:22 +0530

    Times problem solved

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,16 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const startTime = new Date();
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    const endTime = new Date();
+    console.log(`Executed in - ${(endTime-startTime)/1000} sec`);
+    return;
 }
+
+calculateTime(100);
+calculateTime(100000);
+calculateTime(1000000000);
