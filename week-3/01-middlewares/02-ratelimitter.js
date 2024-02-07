4a9a786e91aa41c15b8e74177f7bb0822b3f749a
Yash Tyagi <contactyashtyagi@gmail.com>
2024-02-08 02:04:04 +0530

    rateLimitter done :)

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,19 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+function rateLimitter(req,res,next){
+  const userId = req.header("user-id");
+
+  numberOfRequestsForUser[userId] = (numberOfRequestsForUser[userId] || 0) + 1;
+
+  if(numberOfRequestsForUser[userId] > 5){
+    return res.status(404).json({"message":"Not found"});
+  }
+  next();
+}
+
+app.use(rateLimitter);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
